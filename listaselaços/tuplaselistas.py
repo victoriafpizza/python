d8e770c86737280933975083d79cd0398cd5cf95
victoriafpizza <rm550609@fiap.com.br>
2024-04-23 15:57:02 -0300

    tuplas e listas e suas diferenças

---
+++ listaselaços/tuplaselistas.py
@@ -0,0 +1,5 @@
+# As tuplas são estruturas de dados que nos permitem armazenar elementos de maneira ordenada e sequencial, assim como as listas.
+# Dessa forma, ambas mantêm a ordem dos elementos e oferecem índices para acessar esses valores.
+
+lista = [1,"olá mundo",True,9.7]
+tupla = (1,"olá mundo",True,9.7)
