07e0cd056d64787d53ad69157a479809de1d0f82
victoriafpizza <rm550609@fiap.com.br>
2024-04-23 15:36:17 -0300

    adcionando exercicios do curso de python

---
+++ exercicios/ex1.py
@@ -0,0 +1,10 @@
+# 1 - Solicite ao usuário que insira um número e, em seguida, 
+# use uma estrutura if else para determinar se o número é par ou ímpar.
+
+print("por favor insira um numero")
+num = int(input())
+
+if num / 2:
+    print("seu numero é par")
+else:
+    print("seu numero é impar")
