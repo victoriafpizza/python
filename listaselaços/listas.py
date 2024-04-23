b9c20ade867161cd8ce8b8d215b066b29a04dbec
victoriafpizza <rm550609@fiap.com.br>
2024-04-23 15:55:19 -0300

    adcionando listas

---
+++ listaselaços/listas.py
@@ -0,0 +1,7 @@
+#As listas em Python são usadas para armazenar coleções de itens, como números, strings ou objetos. Elas são muito versáteis e 
+#permitem armazenar múltiplos itens em uma única variável. As listas são mutáveis, o que significa que você pode adicionar, 
+#remover e modificar os itens que elas contêm. Elas são amplamente utilizadas para armazenar e manipular dados em Python.
+
+import os
+
+restaurantes = [] #estrutura das listas
